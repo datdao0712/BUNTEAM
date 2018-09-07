7354c4fe2dae112c7abb71d002e0596dc9586132
viet <viet561995@gmail.com>
2018-09-07 10:22:41 +0700

    pull ve

--- controllers/default.go
+++ controllers/default.go
@@ -7,7 +7,9 @@ import (
 type MainController struct {
 	beego.Controller
 }
-
+type SangController struct {
+	beego.Controller
+}
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
@@ -17,12 +19,15 @@ func (c *MainController) Get() {
 func (c *MainController) Profile() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
-	main.TplName = "viet561995/viet.tpl"
+	c.TplName = "viet561995/viet.tpl"
 }
 
 func (main *MainController) HelloSitepoint() {
-    main.Data["ten"] = "Ten: Hoang"
-    main.Data["team"] = "Bun Team"
-    main.Data["dob"] = "ngay sinh:02/10/1995"
-    main.TplName = "hoang0210/anhbun.tpl"
+	main.Data["ten"] = "Ten: Hoang"
+	main.Data["team"] = "Bun Team"
+	main.Data["dob"] = "ngay sinh:02/10/1995"
+	main.TplName = "hoang0210/anhbun.tpl"
+}
+func (sa *SangController) Get() {
+	sa.TplName = "sang/sang.html"
 }
