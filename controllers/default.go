4ba90fd4b9da737108910559fe9db603c49d1b0c
viet <viet561995@gmail.com>
2018-09-13 22:45:02 +0700

    hkhk
    b

--- controllers/default.go
+++ controllers/default.go
@@ -10,11 +10,15 @@ type MainController struct {
 type SangController struct {
 	beego.Controller
 }
+type HoangController struct {
+	beego.Controller
+}
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
 }
+
 
 func (c *MainController) Profile() {
 	c.Data["Website"] = "beego.me"
@@ -31,3 +35,19 @@ func (main *MainController) HelloSitepoint() {
 func (sa *SangController) Get() {
 	sa.TplName = "sang/sang.html"
 }
+func (login	 *HoangController) Login() {
+	login.TplName = "hoang0210/login.html"
+}
+
+func (show *HoangController) Show(){
+	user := show.GetString("username")
+	pass := show.GetString("pass")
+	show.Data["username"] = user
+	show.Data["pass"] = pass
+	show.TplName = "hoang0210/show.html"
+} 
+
+func (signup *HoangController) Signup(){
+	signup.TplName = "hoang0210/signup.html"
+} 
+
