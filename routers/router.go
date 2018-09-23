6f9b7510dd83a3421009a12038975b066e5143ab
dat.dao <dat.dao@tiki.vn>
2018-09-23 13:47:17 +0700

    Commit

--- routers/router.go
+++ routers/router.go
@@ -7,12 +7,4 @@ import (
 
 func init() {
 	beego.Router("/", &controllers.MainController{})
-	beego.Router("/hoang", &controllers.MainController{}, "get:HelloSitepoint")
-	beego.Router("/viet", &controllers.MainController{}, "get:Profile")
-
-	//hoang
-	beego.Router("/hoang/login", &controllers.HoangController{}, "get:Login")
-	beego.Router("/hoang/show", &controllers.HoangController{}, "post:Show")
-	beego.Router("/signup", &controllers.HoangController{}, "get:Signup")
-	beego.Router("/signup/privacy", &controllers.HoangController{}, "get:Privacy")
 }
